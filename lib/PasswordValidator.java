1c9e3d118891c1d983ab929f258a9333eb79c6ab
WiphadaThabpia <wiphada.th@ku.th>
2025-07-03 17:42:11 +0700

    Update PasswordValidator.java

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -5,15 +5,50 @@ public class PasswordValidator {
     /**
      * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
      * เพื่ออธิบายกฎการทำงานของเมธอด
+     * @param รับค่า....
+     * @return ส่งค่า...
+     * @throws ilegalArgumentException 
      */
     // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
+    public static PasswordStrength validate(String password) 
+    { // Function Type ให้เป็น PasswordStrength 
         
-        // ส่วนของ Implementation ที่คุณต้องเขียน
-        // ...
+        if(password == null || password.length() < 8)
+        {
+            return PasswordStrength.INVALID;
+        }
+
+        int upper = 0;
+        int lower = 0;
+        int digit = 0;
+        for(char c : password.toCharArray())
+        {
+            if(Character.isLowerCase(c))
+            {
+                lower++;
+            }
+            if(Character.isUpperCase(c))
+            {
+                upper++;
+            }
+            if(Character.isDigit(c))
+            {
+                digit++;
+            }
+        }
+        if(lower == password.length())
+        {
+             return PasswordStrength.WEAK;
+        }
+        if(upper == 1)
+        {
+            return PasswordStrength.MEDIUM;
+        }
         
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
-        
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+        if((password.length()> 8)&&(upper > 1)&&(digit >= 1))
+        {
+            return PasswordStrength.STRONG;
+        }
+        return null;
     }
 }
